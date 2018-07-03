361e64548a964bb750336732e5c2e11c19046c81
mercedesb <mercedesbernard@live.com>
2018-07-02 21:54:48 -0500

    removed comments

--- matcher.rb
+++ matcher.rb
@@ -13,7 +13,6 @@ else
   mentor_id = gets.chomp
 end
 
-# 1hilGyMvcKEvXd_a-ZcNtqyb93oBwCgtLmr8fHy2oVM0
 mentor_data = sheets_service.batch_get_values(mentor_id, [DevTogetherMatching::MENTOR_ID_RANGE, DevTogetherMatching::MENTOR_PREFERENCES_RANGE])
 mentor_name_data = mentor_data.value_ranges[0]
 mentor_preferences_data = mentor_data.value_ranges[1]
@@ -25,7 +24,6 @@ else
   mentee_id = gets.chomp
 end
 
-# 1NgOklcdKoLs1hMrtRnGidtp91rNU31xpVj-_Xr6ogcQ
 mentee_data = sheets_service.batch_get_values(mentee_id, [DevTogetherMatching::MENTEE_ID_RANGE, DevTogetherMatching::MENTEE_PREFERENCE_RANGE])
 mentee_name_data = mentee_data.value_ranges[0]
 mentee_preferences_data = mentee_data.value_ranges[1]
