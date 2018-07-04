5a1ab02eac7196b393667ce6795953b74ba33f51
mercedesb <mercedesbernard@live.com>
2018-07-04 11:46:20 -0500

    delegate original matcher.rb logic to dev_together_matching.rb, make main.rb more idiomatic, make code more SOLID

---
+++ main.rb
@@ -0,0 +1,18 @@
+require './services/dev_together_matching'
+
+if ARGV[0]
+  first_sheet_id = ARGV[0]
+else
+  puts 'First Sheet Id:'
+  first_sheet_id = gets.chomp
+end
+
+if ARGV[0]
+  second_sheet_id = ARGV[1]
+else
+  puts 'Second Sheet Id:'
+  second_sheet_id = gets.chomp
+end
+
+matcher = DevTogetherMatching.new(first_sheet_id, second_sheet_id)
+matches = matcher.run
--- services/dev_together_matching.rb
+++ services/dev_together_matching.rb
@@ -1,29 +1,69 @@
+require './services/google_sheets_service'
 require './models/pairing/entity_factory'
+require './models/graph/graph_builder'
+require "./models/graph/max_bipartite_matching"
+require './models/pairing/mentorship_value_calculator'
 
-module DevTogetherMatching
+class DevTogetherMatching
   MENTOR_ID_RANGE = "C2:C50"
   MENTOR_PREFERENCES_RANGE = "D2:D50"
   MENTEE_ID_RANGE = "C2:C50"
   MENTEE_PREFERENCE_RANGE = "F2:F50"
 
-  def self.entity_factory
-    @entity_factory ||= EntityFactory.new
+  def initialize(mentor_sheet_id, mentee_sheet_id)
+    @mentor_sheet_id = mentor_sheet_id
+    @mentee_sheet_id = mentee_sheet_id
+    @sheets_service = GoogleSheetsService.new
+    @entity_factory = EntityFactory.new
+    @value_calculator = MentorshipValueCalculator.new
+    @mentors = []
+    @mentees = []
   end
 
-  def self.get_mentors(name_data, preferences_data)
+  def run
+    @mentors = mentors(mentor_data.value_ranges[0], mentor_data.value_ranges[1])
+    @mentees =  mentees(mentee_data.value_ranges[0], mentee_data.value_ranges[1])
+
+    graph_builder = GraphBuilder.new(@mentors, @mentees, @value_calculator)
+    mentorship_graph = graph_builder.build
+    matching_algorithm = MaxBipartiteMatching.new(mentorship_graph)
+    graph = matching_algorithm.match
+    output(graph.matches)
+  end
+
+  def mentor_data
+    @mentor_data ||= @sheets_service.batch_get_values(@mentor_sheet_id, [MENTOR_ID_RANGE, MENTOR_PREFERENCES_RANGE])
+  end
+
+  def mentee_data
+    @mentee_data ||= @sheets_service.batch_get_values(@mentee_sheet_id, [MENTEE_ID_RANGE, MENTEE_PREFERENCE_RANGE])
+  end
+
+  def mentors(name_data, preferences_data)
     mentors = []
     name_data.values.each_with_index do |name, index|
-      mentors << entity_factory.get_mentor(name, preferences_data.values[index])
+      mentors << @entity_factory.get_mentor(name, preferences_data.values[index])
     end
     mentors
   end
 
-  def self.get_mentees(name_data, preference_data)
+  def mentees(name_data, preference_data)
     mentees = []
     name_data.values.each_with_index do |name, index|
-      mentees << entity_factory.get_mentee(name, preference_data.values[index])
+      mentees << @entity_factory.get_mentee(name, preference_data.values[index])
     end
     mentees
   end
 
+  def output(matches)
+    puts
+    puts 'Pairs'
+    puts matches.map { |pair| pair.to_s }
+    puts
+    puts 'Unmatched Mentors:'
+    puts @mentors.map{|a| a.name} - matches.map{|a| a.head_name}
+    puts
+    puts 'Unmatched Mentees:'
+    puts @mentees.map{|a| a.name} - matches.map{|a| a.tail.name}
+  end
 end
