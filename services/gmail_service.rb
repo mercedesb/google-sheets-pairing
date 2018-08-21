f3697937d27825c3ec924886141ed56787bb7d89
mercedesb <mercedesbernard@live.com>
2018-08-21 17:09:51 -0500

    create base class for shared auth logic

--- services/gmail_service.rb
+++ services/gmail_service.rb
@@ -14,49 +14,18 @@
 
 # https://github.com/google/google-api-ruby-client/blob/master/samples/cli/lib/samples/gmail.rb
 
+require './services/base_google_service'
 require 'google/apis/gmail_v1'
-require 'googleauth'
-require 'googleauth/stores/file_token_store'
-require 'fileutils'
 require 'mail'
 
-class GmailService
-
-  OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
-  CLIENT_SECRETS_PATH = 'client_secret.json'.freeze
-  CREDENTIALS_PATH = 'token.yaml'.freeze
+class GmailService < BaseGoogleService
   SCOPE = Google::Apis::GmailV1::AUTH_SCOPE
 
   def initialize
-    client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
-    token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
-
-    @authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
-    @user_id = 'default'
-
+    super(scope: SCOPE)
     # Initialize the API
     @service = Google::Apis::GmailV1::GmailService.new
-    @service.authorization = authorize
-  end
-
-  ##
-  # Ensure valid credentials, either by restoring from the saved credentials
-  # files or intitiating an OAuth2 authorization. If authorization is required,
-  # the user's default browser will be launched to approve the request.
-  #
-  # @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
-  def authorize
-    credentials = @authorizer.get_credentials(@user_id)
-    if credentials.nil?
-      url = @authorizer.get_authorization_url(base_url: OOB_URI)
-      puts 'Open the following URL in the browser and enter the ' \
-           "resulting code after authorization:\n" + url
-      code = STDIN.gets
-      credentials = @authorizer.get_and_store_credentials_from_code(
-        user_id: @user_id, code: code, base_url: OOB_URI
-      )
-    end
-    credentials
+    @service.authorization = authorize()
   end
 
   def create_draft()
